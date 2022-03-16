3a02d064848e84e94c4bb055dc8c3e49958de476
Hyunggeun Ahn <hahn@c1r9s7.42seoul.kr>
2022-03-16 15:55:05 +0900

    3rd commit

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: hahn <hahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 09:28:02 by hahn              #+#    #+#             */
-/*   Updated: 2022/03/16 14:29:49 by hahn             ###   ########.fr       */
+/*   Updated: 2022/03/16 15:41:39 by hahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,28 +23,32 @@ size_t	ft_strlen(const char *s)
 }
 
 
-void	format_s(char *s)
+int	format_s(char *s)
 {
 	if (!s)
+	{
 		write(1, "(null)", 6);
-/*	else if (!*s)
-		return ;*/
+		return (6);
+	}
+	else if (!*s)
+		return (0);
 	else
 		write(1, s, ft_strlen(s));
-	return ;
+	return ft_strlen(s);
 }
 
-void	format_c(char c)
+int	format_c(char c)
 {
 	write(1, &c, 1);
-	return ;
+	return (1);
 }
 
-void	format_p(unsigned long long p)
+int	format_p(unsigned long long p)
 {
 	char	hex[8];
 	char	*base;
 	int		idx;
+	int		len;
 	
 	base = "0123456789abcdef";
 	idx = 0;
@@ -54,64 +58,65 @@ void	format_p(unsigned long long p)
 		hex[idx++] = base[p % 16];
 		p /= 16;
 	}
-	while (--idx != -1)
-		write(1, &hex[idx], 1);
+	len = --idx;
+	while (idx != -1)
+		write(1, &hex[idx--], 1);
 
-	return ;
+	return (len);
 }
 
-void	format_di(int di)
+int	format_di(int di)
 {
 	int		nbr[10];
 	int		idx;
-	char	output;
+	int		len;
 
 	if (!di)
 	{
 		write (1, "0", 1);
-		return ;
+		return (1);
 	}
-	idx = 0;
-	if (di < 0)
-		write(1, "-", 1);	
-	while (di)
+	else if (di = -2147483648)
 	{
-		nbr[idx++] = di % 10;
-		di /= 10;
+		write (1, "-2147483647", 11);
+		return (11);
 	}
-	while (--idx != -1)
+	else
 	{
-		if (nbr[idx] < 0)
-			output = (nbr[idx] * -1) + '0';
-		else
-			output = nbr[idx] + '0';
-		write(1, &output, 1);
+		if (di < 0)
+			di * -1;
+		
 	}
-	return ;
+	return (len);
 }
 
-void	format_u(unsigned int u)
+int	format_u(unsigned int u)
 {
 	int	nbr[10];
 	int	idx;
+	int	len;
 
+	len = 0;
 	idx = 0;
 	while (u)
 	{
 		nbr[idx++] = u % 10;
 		u /= 10;
 	}
-	while (--idx != -1)
-		write(1, &nbr[idx] + '0', 1);
-	return ;
+	len += --idx;
+	while (idx != -1)
+		write(1, &nbr[idx--] + '0', 1);
+	return (len);
 }
 
-void	format_x(unsigned int x)
+int	format_x(unsigned int x)
 {
 	char	hex[8];
 	char	*base;
 	int		idx;
+	int		len;
 
+	len = 0;
 	base = "0123456789abcdef";
 	idx = 0;
 	while (x)
@@ -119,17 +124,20 @@ void	format_x(unsigned int x)
 		hex[idx++] = base[x % 16];
 		x /= 16;
 	}
-	while (--idx != -1)
-		write(1, &hex[idx], 1);
-	return ;
+	len += --idx;
+	while (idx != -1)
+		write(1, &hex[idx--], 1);
+	return (len);
 }
 
-void	format_X(unsigned int X)
+int	format_X(unsigned int X)
 {
 	char	hex[8];
 	char	*base;
 	int		idx;
+	int		len;
 	
+	len = 0;
 	base = "0123456789ABCDEF";
 	idx = 0;
 	while (X)
@@ -137,43 +145,49 @@ void	format_X(unsigned int X)
 		hex[idx++] = base[X % 16];
 		X /= 16;
 	}
-	while (--idx != -1)
-		write(1, &hex[idx], 1);
-	return ;
+	len += --idx; 
+	while (idx != -1)
+		write(1, &hex[idx--], 1);
+	return (len);
 }
 
-void	contain_specific(va_list ap, const char *format)
+int	contain_specific(va_list ap, const char *format)
 {
 	int	idx;
+	int len;
 
 	idx = 0;
+	len = 0;
 	while (format[idx])
 	{
 		if (format[idx] == '%')
 		{
 			idx++;
 			if (format[idx] == 'c')
-				format_c(va_arg(ap, int));
+				len += format_c(va_arg(ap, int));
 			else if (format[idx] == 's')
-				format_s(va_arg(ap, char *));
+				len += format_s(va_arg(ap, char *));
 			else if (format[idx] == 'p')
-				format_p(va_arg(ap, unsigned long long));
+				len += format_p(va_arg(ap, unsigned long long));
 			else if (format[idx] == 'd' || format[idx] == 'i')
-				format_di(va_arg(ap, int));
+				len += format_di(va_arg(ap, int));
 			else if (format[idx] == 'u')
-				format_u(va_arg(ap, unsigned int));
+				len += format_u(va_arg(ap, unsigned int));
 			else if (format[idx] == 'x')
-				format_x(va_arg(ap, unsigned int));
+				len += format_x(va_arg(ap, unsigned int));
 			else if (format[idx] == 'X')
-				format_X(va_arg(ap, unsigned int));
+				len += format_X(va_arg(ap, unsigned int));
 			else if (format[idx] == '%')
+			{
 				write(1, "%", 1);
+				len++;
+			}
 		}
 		else
 			write(1, &format[idx], 1);
 		idx++;
 	}
-	return ;
+	return (len);
 }
 
 int	ft_printf(const char *format, ...)
@@ -189,35 +203,26 @@ int	ft_printf(const char *format, ...)
 	{
 		if (format[idx] != '%')
 			str_len++;
-		if (format[idx] == '%' && format[idx + 1] == '%')
-		{
-			str_len++;
+		if (format[idx] == '%')
 			idx++;
-		}
 		idx++;
 	}
 	if (ft_strlen(format) == str_len)
 		write(1, format, str_len);
 	else
-		contain_specific(ap, format);
+		str_len += contain_specific(ap, format);
 	va_end(ap);
 	return (str_len);
 }
 
 #include <stdio.h>
-/*
+
 int	main(void)
 {
-	ft_printf("%s", "");
-	ft_printf(" %s", "");
-	ft_printf("%s ", "");
-	ft_printf(" %s ", "");
-	ft_printf(" %s ", "-");
-	ft_printf(" %s %s ", "", "-");
-	ft_printf(" %s %s ", " - ", "");
-	ft_printf(" %s %s %s %s", " - ", "", "4", "");
-	ft_printf(" %s %s %s %s %s ", " - ", "", "4", "", "2 ");
-	ft_printf(" NULL %s NULL ", NULL);
+	ft_printf("%d", 0);
+	ft_printf(" %d", 1);
+	ft_printf("%d ", -1);
+	ft_printf(" %d ", -3);
+	
 }
 
-*/
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,7 +6,31 @@
 /*   By: hahn <hahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 10:24:33 by hahn              #+#    #+#             */
-/*   Updated: 2022/03/16 13:12:31 by hahn             ###   ########.fr       */
+/*   Updated: 2022/03/16 15:53:30 by hahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char	*ft_itoa(unsigned long long n)
+{
+	unsigned long long	temp;
+	int					idx;
+	char				*str;
+
+	if (!n)
+		return ("0");
+	idx = 0;
+	temp = n;
+	while (temp)
+	{
+		idx++;
+		temp /= 10;
+	}
+	str = (char *)malloc(sizeof(char) * (idx + 1));
+	str[idx--] = '\0';
+	while (n)
+	{
+		str[idx--] = n % 10;
+		n /= 10;
+	}
+	return (str);
+}
